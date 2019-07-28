735de0db41c7b5c3c34a037692ff083355cc1406
Michael Freytag <mtfreytag@gmail.com>
2019-07-27 23:23:15 -0500

    Worked through Database Storage Module
    Created a storeSchema. Explicitly defined the Stoe model's Schema and added a pre save method to handle the slug name.
    Created a Mixin for the Store form and a CreateStore method for when the data is posted

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,7 +1,11 @@
-exports.myMiddleware = (req, res, next) => {
-  req.name = 'wes';
-  next();
-}
 exports.homePage = (req, res) => {
   res.render('index');
+}
+
+exports.addStore = (req, res) => {
+  res.render('editStore', { title: '💩 Add Store' });
+}
+
+exports.createStore = (req, res) => {
+  res.json(req.body);
 }
